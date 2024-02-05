6f809873dd710d097145ab49c8f3f7ee806f1a83
LU SIFE <itslusife@gmail.com>
2024-02-05 10:57:54 -0700

    anime :3c

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 loading_text = "Time Travel."
 
-
-
 for (let i = 0; i < loading_text.length; i++) {
     setTimeout(function() {
         document.getElementById("load_text").innerHTML += loading_text[i];
-    }, 300 + (50 * i));
+    }, 1000 + (50 * i));
 };
+
+
+anime.timeline().add({
+    targets: '.bar_left, .bar_right',
+    width: ["0%","30%"],
+    easing: 'easeInOutExpo',
+    duration: 520,
+    delay: 300
+  });
+
+
+anime.timeline().add({
+    targets: '.bar_left, .bar_right',
+    height: ["0%","10%"],
+    easing: "cubicBezier(.5, .05, .1, .9)",
+    duration: 520,
+    delay: 1000
+  });
