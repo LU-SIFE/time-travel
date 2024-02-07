7dc1e22cbb806f2e67144f28dd58db85cf0b4bad
LU SIFE <itslusife@gmail.com>
2024-02-07 11:07:42 -0700

    stuff

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-loading_text = "Time Travel."
+loading_text = [
+  "Time Travel.",
+  "Is it possible?",
+  "Scroll down to find out."
 
-for (let i = 0; i < loading_text.length; i++) {
-    setTimeout(function() {
-        document.getElementById("load_text").innerHTML += loading_text[i];
-    }, 1000 + (50 * i));
-};
+];
+
+disableScroll();
+setTimeout(function() {change_text(0)}, 1000);
+setTimeout(function() {change_text(1)}, 3000);
+setTimeout(function() {change_text(2)}, 5000);
+setTimeout(function() {enableScroll()}, 6500);
+
+
+
+function change_text(index) {
+  //clears text for re-use
+  document.getElementById("load_text").innerHTML = "";
+
+  for (let i = 0; i < loading_text[index].length; i++) {
+    setTimeout(function() {document.getElementById("load_text").innerHTML += loading_text[index][i];}, 50 * i);
+  }
+}
 
 
 anime.timeline().add({
@@ -23,3 +39,28 @@ anime.timeline().add({
     duration: 520,
     delay: 1000
   });
+
+
+  
+function disableScroll() {
+    // Get the current page scroll position in the vertical direction
+   scrollTop =
+       window.pageYOffset || document.documentElement.scrollTop;
+        
+        
+// Get the current page scroll position in the horizontal direction 
+
+ scrollLeft =
+   window.pageXOffset || document.documentElement.scrollLeft;
+   
+   
+  // if any scroll is attempted,
+ // set this to the previous value
+ window.onscroll = function() {
+  window.scrollTo(scrollLeft, scrollTop);
+ };
+}
+
+  function enableScroll() {
+     window.onscroll = function() {};
+ }
