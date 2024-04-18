1a1275d9ef59d088d6170807ea3912ee41bddd2f
LU SIFE <itslusife@gmail.com>
2024-04-18 12:04:29 -0600

    added text

--- index.js
+++ index.js
@@ -67,18 +67,29 @@ function form_disappear() {
 }
 
 var info_array = [
-    'Beginning of Time<br>',
-    'End of Time<br>',
+    '<b>Beginning of Time</b><br>',
+
+    '<b>End of Time</b><hr><br>A popular place, witness the final moments before the destruction of everything, safely through a time loop, and enjoy a dinner at the resteraunt at the end of time™',
+
     '<b>Signing of the Magna Carta</b><hr><br>Travelers often visit this time to witness the signing of one of history\'s most important documents.',
-    'Ignatius tested the first Time Machine<br>',
+
+    '<b>Treaties of Peace</b><hr><br>After the \'Magna Carta\', a series of peace treaties were signed, because of the invention of time travel. Solving conflicts by time travel is an effective path towards world peace.',
+
     '<b>Building of the pyramids</b><hr><br>The pyramids weren\'t built by aliens, they were built by time travelers who traveled back to help with construction.<br><br>Help out today!',
-    'Second branch completed, returned to main timeline<br>',
-    'The Founder learned from previous mistakes.<br>Timeline continued without major incident<br>',
-    'Accidental annihilation of all life',
-    'First timeline successfuly merged back into the timeline<br>',
-    'Third merge attempt.<br> Attempt successful<br>Timeline continued.<br>',
+
+    '<b>Life Preservation Sanction</b><hr><br>Life from another timeline has been relocated to this one, leaving an empty space for testing purposes.',
+
+    '<b>Intergalactic Space Travel</b><hr><br>After major progression, space travel became a thing anyone can do. Visit distant stars and solar systems, in the future.',
+
+    '<b>Accidental Timeline Branch</b><hr><br>This branch of time is disconnected from the others, and is mostly used for research purposes. All life has been carefully transported to another.',
+
+    '<b>World Peace Achieved</b><hr><br>Witness the moment where world peace has finally been achieved, wit hthe signing of the \'Pax mundi\' Treaty.<br><br>Sign your own name!',
+
+    '<b>Intergalactic Peace</b><hr><br>With the signing of the \'spatium pacis\', Intergalactic Peace was established. A safer time to travel the stars has never been known.',
+
     'Disease from the future brought to the past.<br>Timeline reset.<br>',
-    'Second merge attempt started<br> Ignatius Thistlethwaite was lost in this attempt. <br>Timeline reset.<br>',
+
+    '<b>Nuclear Fallout</b><hr><br>After a country broke the treaty, a nuclear fallout occured. If you would like to live in a post-apocalyptic wasteland, this timeline is for you!',
 ];
 
 function info(index) {
